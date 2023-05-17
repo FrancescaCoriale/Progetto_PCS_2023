bc9a6ebf89e084c99f15de5a3b92c3719b9fa07a
Francesca <s283044@studenti.polito.it>
2023-05-17 10:50:20 +0200

    adding files

--- Projects/Raffinamento/src/empty_class.hpp
+++ Projects/Raffinamento/src/empty_class.hpp
@@ -5,10 +5,36 @@
 
 namespace ProjectLibrary
 {
-  class Empty
-  {
+  class Data {
+    private:
+
     public:
-      void Show() const { std::cout<< "Hello world;"<< std::endl; }
+        void Show() const { std::cout<< "Hello world;"<< std::endl; }
+  };
+
+  class SortArea {
+    private:
+      //????
+        struct Pair {
+            int first; //id
+            double second; //Area
+        //ordino vettore di (id,Area) per area maggiore:
+            //modo possibile
+            //bool compareSecondComponent(const vector<int>& a, const vector<int>& b) {
+            //return a[1] > b[1];
+            //}
+            //sort(Aree.begin(), Aree.end(), compareSecondComponent);
+        SortedArea(vector<Pair> Aree);
+    public:
+        //calcolo aree dei triangoli e salvo (id,Area)
+        AreaCalculator(vector<Pair> Aree, Data Input);
+    };
+  };
+
+  class Adjacency {
+    public:
+        Adjacency(vector<unsigned int> SortedA, Data Edges, list<list<unsigned int>> AdjacencyList);
+
   };
 }
 
