f571f191c3d59d3c12026bb8e84b1b67bc83c377
unknown <s283897@studenti.polito.it>
2023-05-24 12:42:33 +0200

    nuovi file

---
+++ Projects/Raffinamento/test/test_MeshUtilities.hpp
@@ -0,0 +1,12 @@
+#ifndef __TEST_MESHUTILITIES_H
+#define __TEST_MESHUTILITIES_H
+
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+TEST(TestMeshUtilities, TestMeshUtilities)
+{
+}
+
+#endif // __TEST_MESHUTILITIES_H
---
+++ Projects/Raffinamento/test/test_Raffinamento.hpp
@@ -0,0 +1,14 @@
+#ifndef __TEST_RAFFINAMENTO_H
+#define __TEST_RAFFINAMENTO_H
+
+#include <gtest/gtest.h>
+#include "MeshUtilities.hpp"
+#include "Raffinamento.hpp"
+using namespace testing;
+using namespace MeshUtilitiesLibrary;
+
+TEST(TestRaffinamento, TestRaffinamento)
+{
+}
+
+#endif // __TEST_RAFFINAMENTO_H
