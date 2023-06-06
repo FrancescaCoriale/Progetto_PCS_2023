831bbcc5c00151f0f36869864aa3c7a60eb865f8
unknown <s283897@studenti.polito.it>
2023-06-06 15:54:26 +0200

    test

---
+++ Projects/Raffinamento/test/test_Point.hpp
@@ -0,0 +1,30 @@
+#ifndef __TEST_POINT_H
+#define __TEST_POINT_H
+
+#include <cmath>
+#include <gtest/gtest.h>
+#include "iostream"
+#include "Raffinamento.hpp"
+#include "Point.hpp"
+#include "Eigen/Eigen"
+#include <algorithm>
+
+using namespace Eigen;
+using namespace RaffinamentoLibrary;
+using namespace testing;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // __TEST_POINT_H
---
+++ Projects/Raffinamento/test/test_Sorting.hpp
@@ -0,0 +1,30 @@
+#define __TEST_SORTING_H
+#define __TEST_SORTING_H
+
+#include <cmath>
+#include <gtest/gtest.h>
+#include "iostream"
+#include "Raffinamento.hpp"
+#include "Point.hpp"
+#include "Eigen/Eigen"
+#include <algorithm>
+
+using namespace Eigen;
+using namespace RaffinamentoLibrary;
+using namespace testing;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // __TEST_SORTING_H
